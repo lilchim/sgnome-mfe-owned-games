c937539d349b1fbb8a9b1fa732d1eff6fe686e96
sgnome <lilchim@protonmail.com>
2025-01-23 23:17:56 -0500

    OwnedGames.svelte
    Moves websocket and state management out of the component

--- src/lib/components/ownedGames.svelte
+++ src/lib/components/ownedGames.svelte
@@ -1,50 +1,27 @@
 <script lang="ts">
     import { state } from '../state/state.svelte'; 
     import { Input } from "$lib/components/ui/input/index";
-    const { ownedGames, loaded } = state;
+    export let sendMessage: (msg: any) => void; // Receive sendMessage from parent
 
-    import { onMount } from 'svelte';
-    import { io, Socket } from 'socket.io-client';
+    let messageToSend = '';
 
-    let socket: Socket;
+    const command = (vanityurl: string) => ({
+        command: 'SGNOME/vanityurl',
+        payload: { vanityurl }
 
-    onMount(() => {
-        console.log('Establishing websocket connection');
-        socket = io('http://localhost:3000'); // Ensure this URL is correct
+    })
 
-        socket.on('connect', () => {
-            console.log('Socket.IO connection established');
-        });
-
-        socket.on('message', (message) => {
-            console.log('Message from server:', message);
-        });
-
-        return () => {
-            socket.disconnect();
-        };
-    });
-
-
-    function sendMessage() {
-        socket.emit('message', 'Hello, Server!');
+    function handleSend() {
+        let cmd = command(messageToSend);
+        sendMessage(cmd); // Call the sendMessage function from $host
     }
 </script>
 
-<button on:click={sendMessage}>Send Message</button>
-
-<h1> Hello World</h1>
-
-State:  {JSON.stringify(state, null, 2)}
-
-{#if loaded}
-
-    {#each ownedGames.games as game}
-        Game
+{#if $state.loaded}
+    {#each $state.ownedGames.games as game}
+        <div>Game: {JSON.stringify(game)}</div>
     {/each}
-
-    {:else}
-    <Input placeholder="Enter Steam Community Vanity ID"/>
-
+{:else}
+    <Input bind:value={messageToSend} placeholder="Enter Steam Community Vanity ID"/>
+    <button on:click={handleSend}>Find Profile</button>
 {/if}
-
