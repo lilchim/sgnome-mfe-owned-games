59364b3cf378a42461439514bd7d8e0319fbf7cd
sgnome <lilchim@protonmail.com>
2025-01-23 23:17:32 -0500

    socket.ts
    Update websocket server to interact with Redis pub/sub

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,7 @@
 import { Server } from 'socket.io';
+import { redisPub, redisSub } from './redisClient'; // Import the Redis client
 
+// Create a new Socket.IO server with CORS enabled. Will need to configure this with env
 const io = new Server({
     cors: {
         origin: "http://localhost:5173", // Allow your Svelte app's origin
@@ -7,17 +9,35 @@ const io = new Server({
     },
 });
 
-io.on('connection', (socket) => {
-    console.log('New client connected:', socket.id);
 
-    socket.on('message', (message) => {
-        console.log(`Received: ${message}`);
-        // Broadcast message to all clients
-        socket.broadcast.emit('message', message);
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    // Subscribe to a Redis channel
+    redisSub.subscribe('SGNOME/Steam/OwnedGames', (err, count) => {
+        if (err) {
+            console.error('Failed to subscribe: %s', err.message);
+        } else {
+            console.log(`Subscribed to ${count} channel(s).`);
+        }
     });
 
+    // Listen for messages from Redis
+    redisSub.on('message', (channel, message) => {
+        console.log(`Received message from Redis on channel ${channel}: ${message}`);
+        // This can be changed to send to a single client if redis knows the client ID that the request originated from
+        socket.emit('message', message); // Send the message to the connected client
+    });
+
+    socket.on('message', (msg) => {
+        console.log('Websocket Server received message');
+        console.dir(msg);
+        let { command, payload } = msg;
+        redisPub.publish(command, JSON.stringify(payload))
+    })
+
     socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
+        console.log('Websocket Client disconnected');
     });
 });
 
